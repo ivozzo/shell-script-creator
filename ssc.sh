32f845f3e53c025b76ee3469803bb7f70ed4ea1c
ivozzo <alessandro.ivaldi@email.it>
2020-03-18 11:01:35 +0100

    Testing some more properties reading

--- ssc.sh
+++ ssc.sh
@@ -4,6 +4,39 @@ scriptPrefix="ssc"
 declare -A properties=()
 
 #
+# Colored message
+#
+#   Show a colored message
+#
+#   params:
+#     1: l_color - Required ANSI color (see http://www.csc.uvic.ca/~sae/seng265/fall04/tips/s265s047-tips/bash-using-colors.html)
+#     2: l_msg - Message to print
+#
+function color_msg() {
+    local l_color="$1"
+    local l_msg="$2"
+    local endColor='\e[0m'
+
+    echo -e "${l_color}$l_msg${endColor}"
+}
+
+#
+# Error
+#
+#   Show an error and quit with exit code 1
+#
+#   params:
+#     1: l_msg - Error message to print
+#
+error() {
+    local l_msg="$1"
+    local red='\e[0;31m'
+
+    color_msg "$red" "Error: $l_msg" 1>&2
+    exit 1
+}
+
+#
 # printProperties
 #
 #   Print properties and / or variables
@@ -11,10 +44,21 @@ declare -A properties=()
 printProperties() {
     echo ""
     echo '################# PROPERTIES #################'
-    #declare -p | grep -vE "^.*(PWD).*$" | grep -E "^.*$scriptPrefix\_.*\_.*$" | awk '{print $3}'
     for property in "${properties[@]}"; do
-            echo $property=${!property}
-        done
+
+        name=$(echo "$property" | cut -d '_' -f3)
+        value=${!property}
+        if echo "$property" | grep -E "^.*Parameters.*$" >/dev/null; then
+            echo "## PARAMETER $name ##"
+            echo "Default value: $(echo "$value" | cut -d ';' -f1)"
+            echo "Mandatory: $(echo "$value" | cut -d ';' -f2 | sed -e "s/0/False/g" -e "s/1/True/g")"
+            echo "Shortcut: -$(echo "$value" | cut -d ';' -f3)"
+            echo "Description: $(echo "$value" | cut -d ';' -f4)"
+            echo ""
+        elif echo "$property" | grep -E "^.*Variables.*$" >/dev/null; then
+            echo "$name"="$value"
+        fi
+    done
 }
 
 #
@@ -24,25 +68,22 @@ printProperties() {
 #
 #   params:
 #     1: l_file - Properties file to read
+#
 readProperties() {
     local l_file="$1"
     local currentSection=""
     local property=""
     local value=""
-
     declare -i index=1
 
     while read -r line; do
 
-        ## Search for section
+        ## Search for sections in properties file and dynamically create variables
         if echo "$line" | grep -E "^\[.*\]$" >/dev/null; then
             currentSection=$(echo "$line" | sed -e "s/\[//g" -e "s/\]//g")
-            echo "Found section $currentSection"
-        elif echo "$line" | grep -E "^.*=.*$" >/dev/null; then
+        elif echo "$line" | grep -E "^[^#].*=.*$" >/dev/null; then
             property=$(echo "$line" | cut -d'=' -f1)
             value=$(echo "$line" | cut -d'=' -f2)
-
-            echo "Found property $property with value $value"
             declare -g "$scriptPrefix"_"$currentSection"_"$property"="$value"
             properties[$index]="$scriptPrefix"_"$currentSection"_"$property"
             ((index++))
